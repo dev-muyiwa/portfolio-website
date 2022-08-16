117bc86249b7bd7b5cc2cf6396bd1f4e3f88d6c4
Moyosoreoluwa <98257761+dev-muyiwa@users.noreply.github.com>
2022-08-16 02:21:18 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,18 +24,18 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 const skillsContent = document.getElementsByClassName('skills_content'),
       skillsHeader = document.querySelectorAll('.skills_header')
 
-function toggleSkills(){
-    let itemClass = this.parentNode.className
-    for(i = 0; i < skillsContent.length; i++){
-        skillsContent[i].className = 'skills_content skills_close'
-    }
-    if(itemClass === 'skills_content skills_close'){
-        this.parentNode.className = 'skills_content skills_open'
-    }
-}
-skillsHeader.forEach((element) =>{
-    element.addEventListener('click', toggleSkills)
-})
+// function toggleSkills(){
+//     let itemClass = this.parentNode.className
+//     for(i = 0; i < skillsContent.length; i++){
+//         skillsContent[i].className = 'skills_content skills_close'
+//     }
+//     if(itemClass === 'skills_content skills_close'){
+//         this.parentNode.className = 'skills_content skills_open'
+//     }
+// }
+// skillsHeader.forEach((element) =>{
+//     element.addEventListener('click', toggleSkills)
+// })
 
 // Swiper JS
 let swiper = new Swiper('.portfolio_container', {
